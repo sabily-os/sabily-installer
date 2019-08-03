5cabeee80da841c6a1a79af33f08b381c73f70a5
Philip Müller <philm@manjaro.org>
2019-08-03 08:28:20 +0200

    [officechooser] initial commit

---
+++ src/modules/officechooser/PackageChooserPage.cpp
@@ -0,0 +1,101 @@
+/* === This file is part of Calamares - <https://github.com/calamares> ===
+ *
+ *   Copyright 2019, Adriaan de Groot <groot@kde.org>
+ *   Copyright 2019, Philip Müller <philm@manjaro.org>
+ *
+ *   Calamares is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Calamares is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Calamares. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "PackageChooserPage.h"
+
+#include "ui_page_package.h"
+
+#include "utils/Logger.h"
+#include "utils/Retranslator.h"
+
+#include <QLabel>
+
+PackageChooserPage::PackageChooserPage( PackageChooserMode mode, QWidget* parent )
+    : QWidget( parent )
+    , ui( new Ui::PackageChooserPage )
+    , m_introduction( QString(),
+                      QString(),
+                      tr( "Office Selection" ),
+                      tr( "Please pick a office suite from the list. The selected product will be installed." ) )
+{
+    m_introduction.screenshot = QPixmap( QStringLiteral( ":/images/choose-office.jpg" ) );
+    cDebug() << m_introduction.screenshot;
+
+    ui->setupUi( this );
+    CALAMARES_RETRANSLATE( updateLabels(); )
+
+    switch ( mode )
+    {
+    case PackageChooserMode::Optional:
+    case PackageChooserMode::Exclusive:
+        ui->products->setSelectionMode( QAbstractItemView::SingleSelection );
+    case PackageChooserMode::Multiple:
+    case PackageChooserMode::RequiredMultiple:
+        ui->products->setSelectionMode( QAbstractItemView::ExtendedSelection );
+    }
+}
+
+void
+PackageChooserPage::currentChanged( const QModelIndex& index )
+{
+    if ( !index.isValid() || !ui->products->selectionModel()->hasSelection() )
+    {
+        ui->productName->setText( m_introduction.name );
+        ui->productScreenshot->setPixmap( m_introduction.screenshot );
+        ui->productDescription->setText( m_introduction.description );
+    }
+    else
+    {
+        const auto* model = ui->products->model();
+
+        ui->productName->setText( model->data( index, PackageListModel::NameRole ).toString() );
+        ui->productScreenshot->setPixmap( model->data( index, PackageListModel::ScreenshotRole ).value< QPixmap >() );
+        ui->productDescription->setText( model->data( index, PackageListModel::DescriptionRole ).toString() );
+    }
+}
+
+void
+PackageChooserPage::updateLabels()
+{
+    if ( ui && ui->products && ui->products->selectionModel() )
+    {
+        currentChanged( ui->products->selectionModel()->currentIndex() );
+    }
+    else
+    {
+        currentChanged( QModelIndex() );
+    }
+    emit selectionChanged();
+}
+
+void
+PackageChooserPage::setModel( QAbstractItemModel* model )
+{
+    ui->products->setModel( model );
+    connect( ui->products->selectionModel(),
+             &QItemSelectionModel::selectionChanged,
+             this,
+             &PackageChooserPage::updateLabels );
+}
+
+bool
+PackageChooserPage::hasSelection() const
+{
+    return ui && ui->products && ui->products->selectionModel() && ui->products->selectionModel()->hasSelection();
+}
